da574dbf6f568abcfc7630f65c0ea15e60f70bd5
SwapnilswapnilKumar <swapnilkumartailor@gmail.com>
2025-07-22 01:09:41 +0530

    bookingRoutes updated

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import { requireAuth } from '@clerk/express';
+
 import { createBooking, getOccupiedSeats } from '../controllers/bookingController.js';
 import { protectUser } from '../middleware/auth.js';
 
-const bookingRouter = express.Router();
+const   bookingRouter = express.Router();
 
-bookingRouter.post('/create', protectUser,createBooking);
+bookingRouter.post('/',requireAuth(),protectUser, createBooking);
 bookingRouter.get('/seats/:showId', getOccupiedSeats);
 
 export default bookingRouter;

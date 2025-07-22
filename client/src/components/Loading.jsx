5951badfebe8cf5ffb2338786f282f4b9a1080c5
SwapnilswapnilKumar <swapnilkumartailor@gmail.com>
2025-07-22 17:00:32 +0530

    loading bar updated

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -2,21 +2,22 @@ import React, { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const Loading = () => {
-
   const { nextUrl } = useParams();
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(nextUrl){
-      setTimeout(()=>{
-        navigate('/' + nextUrl)
-      }, 3000)
+  useEffect(() => {
+    if (nextUrl) {
+      const timer = setTimeout(() => {
+        navigate('/' + nextUrl);
+      }, 3000);
+      return () => clearTimeout(timer);
     }
-  },[])
-  
+  }, [nextUrl, navigate]);
+
   return (
-    <div className='flex justify-center items-center h-[80vh]'>
-        <div className=' animate-spin rounded-full h-14 w-14 border-2 border-t-primary'></div>
+    <div className="flex flex-col justify-center items-center h-[80vh] animate-fadeIn scale-95 animate-duration-700">
+      <div className="animate-spin rounded-full h-16 w-16 border-[6px] border-t-sky-500 border-gray-300 shadow-md"></div>
+      <p className="mt-4 text-gray-600 text-lg animate-pulse">Redirecting, please wait...</p>
     </div>
   )
 }
